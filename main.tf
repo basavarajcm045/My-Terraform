5990cf2b6b750c9b9623a0d22177e93a7d8b3788
basavarajcm045 <154853291+basavarajcm045@users.noreply.github.com>
2025-11-20 13:52:23 +0530

    Update main.tf
    Added s3 backend

--- main.tf
+++ main.tf
@@ -1,19 +1,30 @@
+terraform {
+    backend "s3" {
+        bucket = ""
+        key = "terraform.tfstate"
+        region = "" 
+        encrypt = ""
+        lock = ""
+        #skip_credentials_validation = true
+
+    }
     provider "aws" {
       region = var.aws_region
     }
+}
 
-    module "my_vpc" {
-      source             = "./modules/vpc"
-      vpc_cidr           = var.vpc_cidr
-      public_subnet_cidr = var.public_subnet_cidr
-      name               = "my-app-vpc"
-      availability_zone  = var.availability_zone
-    }
+module "my_vpc" {
+    source             = "./modules/vpc"
+    vpc_cidr           = var.vpc_cidr
+    public_subnet_cidr = var.public_subnet_cidr
+    name               = "my-app-vpc"
+    availability_zone  = var.availability_zone
+}
 
-    module "my_ec2" {
-      source        = "./modules/ec2"
-      ami_id        = var.ami_id
-      instance_type = var.instance_type
-      subnet_id     = module.my_vpc.public_subnet_id
-      name          = "my-app-webserver"
-    }
+module "my_ec2" {
+    source        = "./modules/ec2"
+    ami_id        = var.ami_id
+    instance_type = var.instance_type
+    subnet_id     = module.my_vpc.public_subnet_id
+    name          = "my-app-webserver"
+}
